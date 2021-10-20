e6f72040c55876d3c7ddc0b127ce61d4a186e1af
Lam <LamDuong.inbox@gmail.com>
2021-10-20 06:42:34 -0700

    commit tree ba6bd79a5844417885b2917211e2999dc9d06566

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(str) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let words = str.split(' ');
+    let newStr = [];
+
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i];
+
+      if (i === 0) {
+        newStr.push(this.capitalize(word))
+      } else {
+        if (exceptions.includes(word)) {
+          newStr.push(word)
+        } else {
+          newStr.push(this.capitalize(word))
+        }
+      }
+    }
+
+    return newStr.join(' ');
+  }
 }
